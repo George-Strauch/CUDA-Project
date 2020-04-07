fda83979709ca2d4ab3d248341e94c90a20df832
George Strauch <gestrauch5@gmail.com>
2020-04-06 22:01:48 -0500

    added better documentation

--- Matrix-Multiply/cpu_mm.cpp
+++ Matrix-Multiply/cpu_mm.cpp
@@ -1,30 +1,40 @@
 #include <iostream>
 
-
-
 /*
-written by George Strauch on 4/03/2020
+Written by George Strauch on 4/03/2020
 
-c++ program for matrix multiply using 1d arrays
-a one dimention array is used over 2d to make better use of cache
+c++ program for matrix multiply using 1d arrays.
+A one dimention array is used over 2d to make better use of cache.
+This implementation only uses square matrices as they are much
+easier to debug, calculate and work with, however all functions can work with
+non-square matrices too.
 
-to run:
-g++ cpu_mm.cpp -std=c++17 -O2 -o cpu
-$ time ./cpu 1000 2
+Execution follows the syntax:
+$ ./exec {int matrix_size} {int print_option}
+where the print option can be:
+1: Prints the whole of each matrix for debugging
+and best used with smaller matrices <= 10.
+2: Shows only the first and last element of the result.
+other or no option: does not print anything.
 
+Example run:
+$ g++ cpu_mm.cpp -std=c++17 -O2 -o cpu
+$ time ./cpu 10 1
+$ time ./cpu 1500 2
 */
 
 
-
+// struct to make working with matrices much easier
 struct Matrix
 {
   long long int *values;
-  int rows; // main array size
+  int rows;
   int cols;
 };
 
 
 
+// allocates memory for the matrix
 void init_matrix(Matrix &m)
 {
   m.values = (long long int*)malloc(m.rows*m.cols*sizeof(long long int));
@@ -32,6 +42,7 @@ void init_matrix(Matrix &m)
 
 
 
+// builds the matrix for testing
 void fill_mat(Matrix m)
 {
   for (size_t i = 0; i < m.rows*m.cols; i++) {
@@ -53,18 +64,23 @@ int sum_row(Matrix m1, Matrix m2, int m1_row_ind, int m2_col_ind)
 
 
 
+// multiplies m1 by m2 and returns the resulting matrix
 Matrix matmul(Matrix m1, Matrix m2)
 {
+  // test tomake sure that sizes of m1 and m2 are compatible
+  if(m1.cols != m2.rows){
+    std::cout << "problem in matmul, invalid sizes" << '\n';
+    exit(1);
+  }
+
+  // create the matrix to store the result that will be returned
   Matrix res;
   res.cols = m2.cols;
   res.rows = m1.rows;
   init_matrix(res);
 
-  if(m1.cols != m2.rows){
-    std::cout << "problem in mat mul, invalid sizes" << '\n';
-    exit(1);
-  }
-
+  // every iteration of the inner loop will find one value in the result
+  // outer loop gets each column vector in the result matrix
   for (size_t i = 0; i < m2.cols; i++) {
     for (size_t j = 0; j < m1.rows; j++) {
       res.values[j*res.cols + i] = sum_row(m1, m2, j, i);
@@ -88,6 +104,7 @@ void displayMatrix(Matrix mat)
 
 
 
+// sets a matrix to its transpose, pass by reference
 void transpose(Matrix &mat)
 {
   long long int *new_mat = (long long int*)malloc(mat.rows*mat.cols*sizeof(long long int));
@@ -106,6 +123,7 @@ void transpose(Matrix &mat)
 
 
 
+// makes a copy of a matrix and returns it
 Matrix copy_of_mat(Matrix m)
 {
   Matrix new_mat;
@@ -122,7 +140,7 @@ Matrix copy_of_mat(Matrix m)
 
 int main(int argc, char const *argv[])
 {
-  // the matrix
+  // gets matrix size from user, see header
   Matrix m1;
   m1.rows = atoi(argv[1]);
   m1.cols = atoi(argv[1]);
@@ -131,17 +149,18 @@ int main(int argc, char const *argv[])
 
   std::cout << "rows: " << m1.rows << '\n';
   std::cout << "cols: " << m1.cols << "\n\n";
-  // std::cout << "m1:" << '\n';
-  // displayMatrix(m1);
 
+  // the second matrix is just a copy of the first
   Matrix m2 = copy_of_mat(m1);
-  // std::cout << "m2:" << '\n';
-  // displayMatrix(m2);
+  // transpose(m2);
 
+  // performs matrix multiply
   std::cout << "start" << '\n';
   Matrix result = matmul(m1, m2);
   std::cout << "done\n" << '\n';
 
+  // options to print the matricies
+  // possible options at the top
   if (argc > 2) {
     if (atoi(argv[2]) == 1) {
       std::cout << "m1:" << '\n';
@@ -159,11 +178,10 @@ int main(int argc, char const *argv[])
       std::cout << "first: " << result.values[0] << '\n';
       std::cout << "last: " << result.values[result.cols*result.rows-1] << '\n';
       std::cout << '\n';
-
     }
   }
 
-  // frees all memory
+  // frees the memory
   std::cout << "freeing memory" << '\n';
   free(m1.values);
   free(m2.values);
@@ -171,25 +189,6 @@ int main(int argc, char const *argv[])
 
   return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- Matrix-Multiply/gpu_mm.cu
+++ Matrix-Multiply/gpu_mm.cu
@@ -1,41 +1,59 @@
 #include <iostream>
 
-
 /*
 written by George Strauch on 4/03/2020
 
 c++ program for matrix multiply using 1d arrays on the GPU
 the GPU makes use of parallelism to make processes like this much faster
 
-to run:
-nvcc gpu_mm.cu -o gpu
-$ time ./gpu 1000 2
+This implementation only uses square matrices as they are much
+easier to debug, calculate and work with, however all functions can work with
+non-square matrices too.
 
+this program does not ((yet)) use a struct to make working witht the
+matricies easier as this runs into problems with CUDA however this will change
+
+Execution follows the syntax:
+$ ./exec {int matrix_size} {int print_option}
+where the print option can be:
+1: Prints the whole of each matrix for debugging
+and best used with smaller matrices <= 10.
+2: Shows only the first and last element of the result.
+other or no option: does not print anything.
+
+Example run:
+$ nvcc gpu_mm.cu -o gpu
+$ time ./gpu 10 1
+$ time ./gpu 1500 2
 */
 
 
+
+//calculate a single element of the matrix result of m1*m2
 // res_x = res cols = m2 cols  max
 // res_y = res rows = m1 rows  max
-// common = m1 cols and m2 rows
+// common = m1_cols and m2_rows
 __global__
 void matmul(long long int* m1, long long int* m2, long long int* res, int common, int res_x, int res_y)
 {
+  // gets the id for the x and y position within the result
   int idx = blockIdx.x*blockDim.x + threadIdx.x;
   int idy = blockIdx.y*blockDim.y+ threadIdx.y;
 
+  // there will be some invalid calls of this fuction
+  // this checks to make sure this is calculating a valid element
   if(idx >= res_x || idy >= res_y) { return; }
 
   int id = idy*res_x + idx;
-  int m1_row = res_y * idy;
-
   res[id] = 0;
   for (size_t i = 0; i < common; i++) {
-    res[id] += m1[m1_row+i] * m2[idx + i*res_x];
+    res[id] += m1[res_y*idy+i] * m2[i*res_x+idx];
   }
 }
 
 
 
+// host side function to display matrix
 __host__
 void displayMatrix(long long int *mat, long long int rows, long long int cols)
 {
@@ -50,6 +68,8 @@ void displayMatrix(long long int *mat, long long int rows, long long int cols)
 }
 
 
+
+// host side function to transpose
 __host__
 void transpose(long long int *&mat, long long int rows, long long int cols)
 {
@@ -67,21 +87,26 @@ void transpose(long long int *&mat, long long int rows, long long int cols)
 
 int main(int argc, char const *argv[])
 {
+  // gets the matrix size from user, see header
   int N = atoi(argv[1]);
   std::cout << "N: " << N << '\n';
 
+  // host side matrices
   long long int *m1 = (long long int*)malloc(N*N*sizeof(long long int));
   long long int *m2 = (long long int*)malloc(N*N*sizeof(long long int));
   long long int *rs = (long long int*)malloc(N*N*sizeof(long long int));
 
+  // device side matricies
   long long int *d_m1;
   long long int *d_m2;
   long long int *d_rs;
 
+  // allocate memory for the matrices on the device
   cudaMalloc(&d_m1, N*N*sizeof(long long int));
   cudaMalloc(&d_m2, N*N*sizeof(long long int));
   cudaMalloc(&d_rs, N*N*sizeof(long long int));
 
+  // initalizes the host matricies
   for (size_t a = 0; a < N; a++) {
     for (size_t b = 0; b < N; b++) {
       m1[a*N+b] = b;
@@ -91,18 +116,12 @@ int main(int argc, char const *argv[])
 
   // transpose(m2, N, N);
 
-  // test transpose
-  // std::cout << "before: " << '\n';
-  // displayMatrix(m2, N, N);
-  // std::cout << "\nafter: " << '\n';
-  // transpose(m2, N, N);
-  // displayMatrix(m2, N, N);
-  // std::cout << "\n" << '\n';
-
+  // copies host matrices to devices memory
   cudaMemcpy(d_m1, m1, N*N*sizeof(long long int), cudaMemcpyHostToDevice);
   cudaMemcpy(d_m2, m2, N*N*sizeof(long long int), cudaMemcpyHostToDevice);
 
 
+  // options for building the block grid. Subject to Change.
   //------------------------------------------
   int thdX = 16;
   int thdY = 16;
@@ -110,13 +129,14 @@ int main(int argc, char const *argv[])
   dim3 block_grid((N/thdX)+1, (N/thdY)+1);
   //------------------------------------------
 
-
+  // debug info about the block grid
   std::cout << "----------------------------" << '\n';
   std::cout << "gird x: " << block_grid.x << " grid y: " << block_grid.y << '\n';
   std::cout << "total threads: " << block_grid.x * block_grid.y * thdX * thdY << ", needed: " << N*N << '\n';
-
   std::cout << "----------------------------" << '\n';
 
+  // performs matrix multiply on the GPU, each thread will handle one element
+  // then copys the result to host memory
   std::cout << "\nstart" << '\n';
   matmul<<<block_grid, threads_in_block>>>(d_m1, d_m2, d_rs, N, N, N);
   cudaMemcpy(rs, d_rs, N*N*sizeof(long long int), cudaMemcpyDeviceToHost);
@@ -124,6 +144,7 @@ int main(int argc, char const *argv[])
 
 
 // display array
+// display options listed in header
   if (argc > 2) {
     if (atoi(argv[2]) == 1) {
       std::cout << "matrix 1: " << '\n';
@@ -143,8 +164,8 @@ int main(int argc, char const *argv[])
     }
   }
 
+  // frees device and host memeory
   std::cout << "freeing memory" << '\n';
-
   cudaFree(d_m1);
   cudaFree(d_m2);
   cudaFree(d_rs);
