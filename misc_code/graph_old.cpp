bf6e2c625524b89ca41432997318d3b968d03d00
George Strauch <gestrauch5@gmail.com>
2020-05-06 21:26:39 -0500

    update

--- misc_code/graph_old.cpp
+++ misc_code/graph_old.cpp
@@ -5,8 +5,7 @@
 // edges is an array of edges where every even index is the start
 // vertex and the next (odd) index is the end vertex
 struct Graph {
-    int* edges_start;
-    int* edges_end;
+    int* edges;
     int num_edges;
     int num_verticies;
 };
@@ -46,14 +45,14 @@ bool is_matching(Graph g, bool* ba)
     if(ba[i])
     {
       for (size_t j = 0; j < index; j++) {
-        if(g.edges_start[i] == known[j] || g.edges_end[i] == known[j]) {
+        if(g.edges[2*i] == known[j] || g.edges[2*i+1] == known[j]) {
           free(known);
           return false;
         } // end if
       }// end inner for
 
-      known[index] = g.edges_start[i];
-      known[index+1] = g.edges_end[i];
+      known[index] = g.edges[2*i];
+      known[index+1] = g.edges[2*i+1];
       index+=2;
     }// end if
   }// end for
@@ -68,8 +67,8 @@ void graph_print(Graph g) {
   for (size_t v = 0; v < g.num_verticies; v++) {
     std::cout << "vertex " << v << " --> ";
     for (size_t i = 0; i < g.num_edges; i++) {
-      if (g.edges_start[i] == v) {
-        std::cout << g.edges_end[i] << ", ";
+      if (g.edges[2*i] == v) {
+        std::cout << g.edges[2*i+1] << ", ";
       }
     }
     std::cout << '\n';
@@ -87,8 +86,7 @@ Graph gen_random_graph(int num_verticies, int num_edges)
   Graph g;
   g.num_edges = num_edges;
   g.num_verticies = num_verticies;
-  g.edges_start = (int*)malloc(num_edges*sizeof(int));
-  g.edges_end = (int*)malloc(num_edges*sizeof(int));
+  g.edges = (int*)malloc(2*num_edges*sizeof(int));
 
   int start, end, tmp, iter;
   bool again;
@@ -111,14 +109,14 @@ Graph gen_random_graph(int num_verticies, int num_edges)
         }
 
         // edge not already exists
-        if(start == g.edges_start[j] && end == g.edges_end[j]) {
+        if(start == g.edges[2*j] && end == g.edges[2*j +1]) {
           end = (end+1) % num_verticies;
           again = true;
           break;
         }
 
         // reverse edge not already exists
-        if(end == g.edges_start[j] && start == g.edges_end[j]) {
+        if(end == g.edges[2*j] && start == g.edges[2*j +1]) {
           end = (end+1) % num_verticies;
           again = true;
           break;
@@ -126,8 +124,8 @@ Graph gen_random_graph(int num_verticies, int num_edges)
       }
 
       if (!again) {
-        g.edges_start[i] = start;
-        g.edges_end[i] = end;
+        g.edges[2*i] = start;
+        g.edges[2*i + 1] = end;
       }
       else {
         if (iter % num_verticies == 0 && iter != 0) {
@@ -140,10 +138,10 @@ Graph gen_random_graph(int num_verticies, int num_edges)
 
   // make the graph nicer
   for (size_t k = 0; k < num_edges; k++) {
-    if (g.edges_start[k] > g.edges_end[k]) {
-      tmp = g.edges_start[k];
-      g.edges_start[k] = g.edges_end[k];
-      g.edges_end[k] = tmp;
+    if (g.edges[2*k] > g.edges[2*k+1]) {
+      tmp = g.edges[2*k];
+      g.edges[2*k] = g.edges[2*k+1];
+      g.edges[2*k+1] = tmp;
     }
   }
 
@@ -155,38 +153,44 @@ Graph gen_random_graph(int num_verticies, int num_edges)
 // generates a predictible graph
 Graph gen_known_graph(int n)
 {
-
   Graph g;
   g.num_edges = 3*n;
   g.num_verticies = 2*n;
+  g.edges = (int*)malloc(2*g.num_edges*sizeof(int));
 
-  g.edges_start = (int*)malloc(3*n*sizeof(int));
-  g.edges_end = (int*)malloc(3*n*sizeof(int));
+  int *start = (int*)malloc(3*n*sizeof(int));
+  int *end = (int*)malloc(3*n*sizeof(int));
 
-  // main loop
   for (size_t i = 0; i < n; i++) {
-    g.edges_start[i] = i;
-    g.edges_end[i] = (i+1) % n;
+    start[i] = i;
+    end[i] = (i+1) % n;
   }
 
-  // main to outter
   for (size_t i = 0; i < n; i++) {
-    g.edges_start[i+n] = i;
-    g.edges_end[i+n] = i+n;
+    start[i+n] = i;
+    end[i+n] = i+n;
   }
 
-  // outter to main
   for (size_t i = 0; i < n; i++) {
-    g.edges_start[i+2*n] = i+n;
-    g.edges_end[i+2*n] = (i+1) % n;
+    start[i+2*n] = i+n;
+    end[i+2*n] = (i+1) % n;
   }
 
+  for (size_t i = 0; i < 3*n; i++) {
+    g.edges[2*i] = start[i];
+    g.edges[2*i+1] = end[i];
+  }
   return g;
 }
 
 
 
 int main(int argc, char const *argv[]) {
+  //
+  // Graph g = gen_random_graph(10, 4);
+  // graph_print(g);
+  //
+  // std::cout << '\n';
 
   Graph gg = gen_known_graph(atoi(argv[1]));
   graph_print(gg);
@@ -200,20 +204,6 @@ int main(int argc, char const *argv[]) {
     }
   }
   std::cout << mathcings << '\n';
-
-
-
-
-
-
-std::cout << "\n" << '\n';
-
-Graph h = gen_random_graph(10, 30);
-graph_print(h);
-
-
-
-
   return 0;
 }
 
